c8d296f3e6eee45d8f36cbfede17b2a993c53001
Glaucio Daniel <glauciodaniel@gmail.com>
2022-10-27 14:21:19 -0300

    Deploy

---
+++ typeorm/migration/1666891219942-users.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class users1666891219942 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
