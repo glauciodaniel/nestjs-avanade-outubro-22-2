c562487891db87ca79a3c45ac820b69c7ce55ac9
Glaucio Daniel <glauciodaniel@gmail.com>
2022-10-28 15:51:42 -0300

    Deploy

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { users } from '@prisma/client';
 import { PrismaService } from '../prisma.service';
 
@@ -6,21 +6,43 @@ import { PrismaService } from '../prisma.service';
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
+  async verifyUserExists(email: string): Promise<boolean> {
+    const user = await this.prisma.users.findFirst({
+      where: {
+        email,
+      },
+    });
+    if (user) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Email já cadastrado',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return false;
+  }
+
   async createUser(data): Promise<users> {
     const { name, email, password } = data;
 
-    const user = await this.prisma.users.create({
-      data: {
-        name,
-        email,
-        password,
-      },
-    });
+    const checkUser = await this.verifyUserExists(email);
 
-    if (!user) {
-      throw new Error('Erro ao criar usuário');
+    if (!checkUser) {
+      const user = await this.prisma.users.create({
+        data: {
+          name,
+          email,
+          password,
+        },
+      });
+
+      if (!user) {
+        throw new Error('Erro ao criar usuário');
+      }
+      return user;
     }
-    return user;
   }
 
   async findAll() {
